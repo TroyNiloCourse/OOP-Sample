6bf5f672dd96e62648407438d2002d8d6c2cb579
TroyNilo <alllose2troy@gmail.com>
2021-05-08 17:04:26 +0800

    OOP Sample Project

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string FullName 
+        {
+            get
+            {
+                return LastName += FirstName;
+            }
+        }
+        public int CustomerID { get; private set; }
+        public string EmailAdress { get; set; }
+        public string FirstName { get; set; }
+        private string _lastname;
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+            }
+        }
+    }
+}
